de6f4366c61a8105948124409184b40b219a7e82
setgoogle <setisdale@google.com>
2020-07-06 17:53:38 +0000

    moving code from groceryDatabase branch

--- src/main/java/com/google/sps/servlets/GroceryServlet.java
+++ src/main/java/com/google/sps/servlets/GroceryServlet.java
@@ -27,39 +27,108 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+// TODO: fix git error when pushing
 /** Servlet takes grocery list and displays it to the screen based on the users family */
 @WebServlet("/grocery-list")
 public class GroceryServlet extends HttpServlet {
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  private static final String GROCERY = "Grocery";
+  private static final String FAMILY_ID = "familyID";
 
-    // TODO: add items to the specific families datastore.   
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {      
+    UserService userService = UserServiceFactory.getUserService();
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    String userEmail = userService.getCurrentUser().getEmail();
+
+    // if user isn't associated with family don't allow them to create a list
+    Query query = new Query("UserInfo")
+        .setFilter(new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, userEmail));
+    PreparedQuery results = datastore.prepare(query);
+    Entity userInfoEntity = results.asSingleEntity();
+    if (userInfoEntity == null) {
+        response.sendRedirect("/grocery.html");
+        return;
+    }
+    String assignGrocery = request.getParameter("assignGrocery");
+    String noneAssigned = " ";
+    Entity groceryEntity = new Entity(GROCERY);
+   
+    // turn familyID into a key so the users family can be accessed
+    long familyID = (long) userInfoEntity.getProperty(FAMILY_ID);
+    Key familyEntityKey = KeyFactory.createKey("Family", familyID);
+    Entity familyEntity;
+    try {
+        familyEntity = datastore.get(familyEntityKey);
+    } catch (EntityNotFoundException e) {
+        System.out.println("Family not found");
+        return;
+    }
+    System.out.println("grocery assigned to: " + assignGrocery);
+    // checks if the given email matches a email in the family
+    if (assignGrocery.equals("") || assignGrocery.equals(null)) {
+        groceryEntity.setProperty("assignEmail", noneAssigned);
+    } else {
+        ArrayList<String> memberEmails = (ArrayList<String>) familyEntity.getProperty("memberEmails");
+        for(int i = 0; i < memberEmails.size(); i++) {
+            if(assignGrocery.equals(memberEmails.get(i))) {
+                groceryEntity.setProperty("assignEmail" , memberEmails.get(i));
+                break;
+            } else if(i == memberEmails.size() - 1 ) {
+                response.sendRedirect("/grocery.html");
+                return;
+            }
+        }
+    }
+    
     // adds item to datastore
     String grocery = request.getParameter("groceryItem");
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Entity groceryEntity = new Entity("Grocery");
     if( grocery != null && !grocery.equals("")) {
         groceryEntity.setProperty("grocery", grocery);
+        groceryEntity.setProperty(FAMILY_ID, familyID);
         datastore.put(groceryEntity);
-    }
-               
-    doGet(request,response);  
+    }     
     response.sendRedirect("/grocery.html");
   }
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      // creates arraylist and starts query
-      ArrayList<String> groceryList = new ArrayList<String>();
-      Query query = new Query("Grocery");
-      PreparedQuery results = datastore.prepare(query);
-        
-      //loads entities into arraylist to be printed
-      for (Entity entity : results.asIterable()) {
-          String groceryItem = (String) entity.getProperty("grocery");
-        groceryList.add(groceryItem);
-      }
-      Gson gson = new Gson();
-      response.setContentType("application/json;");
-      response.getWriter().println(gson.toJson(groceryList));
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    UserService userService = UserServiceFactory.getUserService();
+
+    // checks if user is a part of a family and returns an error if they aren't
+    String userEmail = userService.getCurrentUser().getEmail();
+    Query query = new Query("UserInfo")
+        .setFilter(new Query.FilterPredicate("email", Query.FilterOperator.EQUAL, userEmail));
+    PreparedQuery results = datastore.prepare(query);
+    Entity userInfoEntity = results.asSingleEntity();
+    if (userInfoEntity == null) {
+        response.setContentType("text/html;");
+        response.getWriter().println("You do not belong to a family yet!");
+    return;
     }
+
+    // creates arraylist and starts query
+    ArrayList<String> groceryList = new ArrayList<String>();
+    long familyID = (long) userInfoEntity.getProperty(FAMILY_ID);
+    Query groceryQuery = new Query(GROCERY)
+      .setFilter(new Query.FilterPredicate(FAMILY_ID, Query.FilterOperator.EQUAL, familyID));   
+    PreparedQuery familyGrocery= datastore.prepare(groceryQuery);
+    groceryList = checkGroceries(familyGrocery);   
+
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(groceryList));
+    }
+    // returns items from the query that match the users familyID
+  private ArrayList<String> checkGroceries(PreparedQuery familyGrocery) {
+    ArrayList<String> groceryList = new ArrayList<String>();
+    for (Entity entity : familyGrocery.asIterable()) {
+        String groceryOutput;
+            String groceryItem = (String) entity.getProperty("grocery");
+            String memberEmail = (String) entity.getProperty("assignEmail");
+            groceryOutput = groceryItem + " assigned to: " + memberEmail;
+            groceryList.add(groceryOutput);
+    }
+    return groceryList;
+  }
 }
