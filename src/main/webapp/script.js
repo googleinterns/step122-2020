4196a3d48215c298273443713c297d01ebc8a4e7
Anthony Zhu <aqzhu@umich.edu>
2020-07-07 21:12:58 +0000

    Add frontend code to enable creating a calendar

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -101,15 +101,24 @@ function insertCalendar() {
     const calElement = document.getElementById('caldiv');
 
     fetch('/calendar').then((response) => response.text()).then((calSrc) => {
+        if(!calSrc || 0 === calSrc.length || !calSrc.trim()) {
+            return;
+        }
         var calFrame = document.createElement('iframe');
         calFrame.setAttribute('src', calSrc);
         calFrame.setAttribute('style', 'border: 0'); 
         calFrame.setAttribute('width', '800'); 
         calFrame.setAttribute('height', '600'); 
         calFrame.setAttribute('frameborder', '0'); 
-        calFrame.setAttribute('scrolling', 'no'); 
-        calElement.appendChild(calFrame)
+        calFrame.setAttribute('scrolling', 'no');
+
         calElement.appendChild(calFrame);
     });
 
+}
+
+function createCalendar() {
+    fetch(new Request('/new-calendar', {method: 'POST'})).then(() => {
+        insertCalendar();
+    });
 }
